97b4ef47c97c2b1bad22307e24dfa499099daae8
gnever <gaobin32643@126.com>
2014-04-25 16:21:35 +0800

    api

---
+++ base.php
@@ -0,0 +1,16 @@
+<?php
+$PROMO_NAME = 'zhifubao';
+require '/var/promo/base.php';
+/*主文件内容
+global $base_path;
+$base_path = '/var/promo/' . $PROMO_NAME . '/';
+
+function __autoload($class_name) {
+    if(trim($class_name)) {
+        $path = exec("find {$zhifubao}class/ -name {$class_name}.php");
+        if($path && is_file($path)) {
+            require $path;
+        }
+    } 
+}
+ */
